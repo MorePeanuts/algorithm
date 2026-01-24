1f18ba70959157c7096be841398de569d3ada077
Leo <liaofeng0203@gmail.com>
2026-01-24 18:51:55 +0800

    Fix document formatting issues and improve the formatting of unit tests.

--- leetcode/0201-0300/0217_Contains_Duplicate/solution_test.go
+++ leetcode/0201-0300/0217_Contains_Duplicate/solution_test.go
@@ -4,20 +4,32 @@ import "testing"
 
 func TestContainsDuplicates(t *testing.T) {
 	tests := []struct {
-		name     string
-		nums     []int
-		expected bool
+		name string
+		nums []int
+		want bool
 	}{
-		{"true case 1", []int{1, 2, 3, 1}, true},
-		{"false case 1", []int{1, 2, 3, 4}, false},
-		{"true case 2", []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}, true},
+		{
+			"true case 1",
+			[]int{1, 2, 3, 1},
+			true,
+		},
+		{
+			"false case 1",
+			[]int{1, 2, 3, 4},
+			false,
+		},
+		{
+			"true case 2",
+			[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2},
+			true,
+		},
 	}
 
 	for _, tst := range tests {
 		t.Run(tst.name, func(t *testing.T) {
-			actual := containsDuplicate(tst.nums)
-			if actual != tst.expected {
-				t.Errorf("containsDuplicate(%v) = %v, want %v", tst.nums, actual, tst.expected)
+			got := containsDuplicate(tst.nums)
+			if got != tst.want {
+				t.Errorf("containsDuplicate(%v) = %v, want %v", tst.nums, got, tst.want)
 			}
 		})
 	}
--- leetcode/0201-0300/0242_Valid_Anagram/solution_test.go
+++ leetcode/0201-0300/0242_Valid_Anagram/solution_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestIsAnagram(t *testing.T) {
 	tests := []struct {
-		name     string
-		s        string
-		t        string
-		expected bool
+		name string
+		s    string
+		t    string
+		want bool
 	}{
 		{"pos case", "anagram", "nagaram", true},
 		{"neg case", "rat", "car", false},
@@ -15,9 +15,9 @@ func TestIsAnagram(t *testing.T) {
 
 	for _, tst := range tests {
 		t.Run(tst.name, func(t *testing.T) {
-			actual := isAnagram(tst.s, tst.t)
-			if actual != tst.expected {
-				t.Errorf("isAnagram(%v, %v) = %v, want %v", tst.s, tst.t, actual, tst.expected)
+			got := isAnagram(tst.s, tst.t)
+			if got != tst.want {
+				t.Errorf("isAnagram(%v, %v) = %v, want %v", tst.s, tst.t, got, tst.want)
 			}
 		})
 	}
